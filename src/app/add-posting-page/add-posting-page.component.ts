60e8550fa661ebd84038296fcad92719ab36f61d
therepo90 <t.szepczynski@gmail.com>
2018-09-08 14:34:27 +0200

    basic validators

--- src/app/add-posting-page/add-posting-page.component.ts
+++ src/app/add-posting-page/add-posting-page.component.ts
@@ -1,7 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
+import {post} from 'selenium-webdriver/http';
 import {PostingsService} from '../postings/services/postings.service';
+
+interface Control {
+  field: string;
+  ctrl: FormControl;
+  label: string;
+}
 
 @Component({
   selector: 'app-add-posting-page',
@@ -11,7 +18,7 @@ import {PostingsService} from '../postings/services/postings.service';
 export class AddPostingPageComponent implements OnInit {
 
   form: FormGroup;
-  controls: Array<any>;
+  controls: Array<Control>;
 
   constructor(private router: Router, private postingService: PostingsService) { }
 
@@ -19,11 +26,13 @@ export class AddPostingPageComponent implements OnInit {
     this.controls = [
       {field: 'title', ctrl: new FormControl('', Validators.required), label: 'Title'},
       {field: 'company', ctrl:  new FormControl('', Validators.required), label: 'Company' },
-      {field: 'salaryMin', ctrl:  new FormControl('', Validators.required), label: 'Salary min' },
+      {field: 'salaryMin', ctrl:  new FormControl('', Validators.required,), label: 'Salary min' },
       {field: 'salaryMax', ctrl:  new FormControl('', Validators.required), label: 'Salary max' },
       {field: 'city', ctrl:  new FormControl('', Validators.required), label: 'City' },
       {field: 'street', ctrl:  new FormControl('', Validators.required), label: 'Street' },
-      {field: 'postalCode', ctrl:  new FormControl('', Validators.required), label: 'Postal code' },
+      {field: 'postalCode', ctrl:  new FormControl('',
+          [Validators.required, Validators.pattern(/^\d\d-\d\d\d$/)]
+        ), label: 'Postal code' },
     ];
 
     const formControls = this.controls.reduce((prev, curr) => (
@@ -40,7 +49,6 @@ export class AddPostingPageComponent implements OnInit {
   }
 
   onSubmit() {
-    const posting = {};
-    // this.postingService.addPosting()
+     this.postingService.addPosting(this.form.getRawValue());
   }
 }
