7dee57d2583488aeb8367df328d8bd7dd0ba4949
therepo90 <t.szepczynski@gmail.com>
2018-09-08 13:24:04 +0200

    cssing

--- src/app/form-page/form-page.component.ts
+++ src/app/form-page/form-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-form-page',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  onBack() {
+    this.router.navigateByUrl('/postings');
+  }
 }
--- src/app/form-page/form-page.module.ts
+++ src/app/form-page/form-page.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {ClarityModule} from '@clr/angular';
 import {FormPageComponent} from './form-page.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ClarityModule,
+    RouterModule,
   ],
   declarations: [FormPageComponent],
   exports: [FormPageComponent],
